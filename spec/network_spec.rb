16f5e4972c1f8964c04b587e301a87006ce5087a
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-21 12:50:28 -0400

    Add test for show by actor

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -49,10 +49,10 @@ RSpec.describe Show do
     expect(nbc.main_characters).to eq([michael_knight, kitt, leslie_knope, ron_swanson])
   end
 
-  it 'has main characters' do
+  it 'determines actors_by_show' do
     nbc = Network.new("NBC")
-    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
     leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
@@ -60,5 +60,22 @@ RSpec.describe Show do
     nbc.add_show(knight_rider)
     nbc.add_show(parks_and_rec)
     expect(nbc.actors_by_show).to eq({knight_rider => ["David Hasselhoff", "William Daniels"], parks_and_rec => ["Amy Poehler", "Nick Offerman"]})
+  end
+
+  xit 'determines show_by_actors' do
+    nbc = Network.new("NBC")
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    baywatch = Show.new("Baywatch", "Gregory Bonann", [mitch])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    nbc.add_show(knight_rider)
+    nbc.add_show(baywatch)
+    nbc.add_show(parks_and_rec)
+    expect(nbc.shows_by_actor).to eq({"David Hasselhoff" => [knight_rider, baywatch], "William Daniels" => [knight_rider], "Amy Poehler" => [parks_and_rec],"Nick Offerman" => [parks_and_rec]})
   end
 end
